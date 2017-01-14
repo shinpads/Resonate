b7b53021d855c655b5f698ae48531e77c82a5bd7
marshmelllo <robfarlow@gmail.com>
2017-01-14 11:30:17 -0500

    added java.sql jar

---
+++ LocalMusicScene/app/src/main/java/lemonapps/localmusicscene/SQLConnection.java
@@ -0,0 +1,13 @@
+package lemonapps.localmusicscene;
+import java.sql.*;
+/**
+ * Created by Rob on 1/12/2017.
+ */
+public class SQLConnection {
+    Connection connection = null;
+    String ip = "";
+    String dataBaseName = "";
+    String userName = "";
+    String password = "";
+    String driverClass = "net.sourceforge.jtds.jdbc.Driver";
+}
