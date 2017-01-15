109754508c5f465a76e8c2183e712783d4b838da
marshmelllo <robfarlow@gmail.com>
2017-01-14 23:29:56 -0500

    Changed hashing algorithm, imported javax.mail

---
+++ LocalMusicScene/app/src/main/java/lemonapps/localmusicscene/EmailSend.java
@@ -0,0 +1,11 @@
+package lemonapps.localmusicscene;
+
+/**
+ * Created by Rob on 1/14/2017.
+ */
+import java.util.*;
+import javax.mail.*;
+import javax.mail.internet.*;
+
+public class EmailSend {
+}
