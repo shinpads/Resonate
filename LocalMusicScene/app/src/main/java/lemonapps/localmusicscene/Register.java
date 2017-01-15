7de09249cdcaa62235ec850265a1125fdf0891ce
marshmelllo <robfarlow@gmail.com>
2017-01-14 20:16:17 -0500

    setup basics for login.class

---
+++ LocalMusicScene/app/src/main/java/lemonapps/localmusicscene/Register.java
@@ -0,0 +1,16 @@
+package lemonapps.localmusicscene;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Register extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.signup);
+        SQLConnection sqlCon = new SQLConnection();
+
+
+    }
+}
--- LocalMusicScene/app/src/main/java/lemonapps/localmusicscene/SQLConnection.java
+++ LocalMusicScene/app/src/main/java/lemonapps/localmusicscene/SQLConnection.java
@@ -10,41 +10,35 @@ import java.sql.*;
  * Created by Rob on 1/12/2017.
  */
 public class SQLConnection {
-    //public Connection connection = null;
-    private static final String ip = "192.168.2.183:1433"  ; //192.68.2.183
-    private static final String dataBaseName = "LOCALMUSICSCENE";
+    private Connection con;
+    private static final String ip = "184.146.28.136:1433";
+    private static final String database = "master";
     private static final String username = "Java";
     private static final String password ="thesolohoarder@123";
-    private static  String driverClass = "net.sourceforge.jtds.jdbc.Driver";
-    private static final String url = "jdbc:jtds:sqlserver://192.168.2.183:1433/master;integratedSecurity=true";
-    //private static final String url = "jdbc:jtds:sqlserver://" + ip + ";" + "databaseName=" + dataBaseName + ";user=" + username + ";password=" + password + ";";
+    private static final String driverClass = "net.sourceforge.jtds.jdbc.Driver";
+    private static final String url = "jdbc:jtds:sqlserver://"+ip+"/"+database+";integratedSecurity=true";
+
     public SQLConnection() {
-        ConnectToDataBase();
+        con = ConnectToDataBase();
     }
 
     @SuppressLint("NewAPI")
-    public static Connection ConnectToDataBase(){
+    private static Connection ConnectToDataBase(){
         Connection conn = null;
         StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
         StrictMode.setThreadPolicy(policy);
         try {
             Class.forName(driverClass);
             conn = DriverManager.getConnection(url,username,password);
-            Statement statement = conn.createStatement();
-            ResultSet resultSet = statement.executeQuery("SELECT * FROM Signup");
-            if(resultSet.isBeforeFirst()){
-                Log.e("SQL","NO DATA");
-            }else{
-                Object resObj = resultSet.getObject(1);
-                if(resObj instanceof String){
-                    Log.i("SQLTABLE",(String)resObj);
-                }
-            }
-
         }catch(Exception ex) {
             Log.e("SQLERROR",ex.getLocalizedMessage());
         }
         return conn;
+    }
+
+    public static boolean CheckLogin(String user, String pass) {
+
+        return false;
     }
 }
 
