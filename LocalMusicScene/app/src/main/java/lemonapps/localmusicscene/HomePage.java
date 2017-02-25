e24d041c9f24772a84877e157ad7187fff1da4a4
marshmelllo <robfarlow@gmail.com>
2017-02-25 14:24:11 -0500

    Google Places gets Location

--- LocalMusicScene/app/src/main/java/lemonapps/localmusicscene/HomePage.java
+++ LocalMusicScene/app/src/main/java/lemonapps/localmusicscene/HomePage.java
@@ -3,19 +3,26 @@ package lemonapps.localmusicscene;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.location.places.Place;
+import com.google.android.gms.location.places.Places;
+import com.google.android.gms.location.places.ui.PlaceAutocomplete;
+
 public class HomePage extends AppCompatActivity {
-    TextView changeLocationBtn;
+    TextView locationTxt;
+    static final int placeAutoCompleteReqestCode = 1;
+    String location;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_page);
-        changeLocationBtn = (TextView) findViewById(R.id.taxbarLocationText);
-        changeLocationBtn.setOnClickListener(new View.OnClickListener() {
+        locationTxt = (TextView) findViewById(R.id.taxbarLocationText);
+        locationTxt.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 placesAutoComplete();
@@ -23,7 +30,26 @@ public class HomePage extends AppCompatActivity {
         });
     }
     private void placesAutoComplete(){
-        Toast.makeText(getApplicationContext(),"Change Location!",Toast.LENGTH_SHORT).show();
 
+      try {
+          Intent i = new PlaceAutocomplete.IntentBuilder(PlaceAutocomplete.MODE_OVERLAY).build(this);
+          startActivityForResult(i,placeAutoCompleteReqestCode);
+
+      }catch (Exception ex){
+          Log.e("Places Error",ex.getMessage());
+      }
     }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data){
+        if(requestCode == placeAutoCompleteReqestCode){
+            if(resultCode == RESULT_OK){
+                Place place = PlaceAutocomplete.getPlace(this, data);
+                location = place.getName().toString();
+                locationTxt.setText(location);
+            }
+        }
+        super.onActivityResult(requestCode,resultCode,data);
+    }
+
+
 }
