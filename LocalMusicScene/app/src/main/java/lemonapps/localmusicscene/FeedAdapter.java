07dff9b6b330bab0f06f16f4c729223e2367b2cc
mootgoat <david.41hz@protonmail.com>
2017-03-14 13:19:40 -0400

    FeedAdapter class

---
+++ LocalMusicScene/app/src/main/java/lemonapps/localmusicscene/FeedAdapter.java
@@ -0,0 +1,66 @@
+package lemonapps.localmusicscene;
+
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.util.List;
+
+
+/**
+ * Created by Rob on 2017-03-14.
+ */
+
+public class FeedAdapter extends RecyclerView.Adapter<FeedAdapter.ViewHolder>{
+    private List<FeedItem> feedItemList;
+    private Context context;
+    public FeedAdapter(Context cont, List<FeedItem> feedlist){
+        this.feedItemList = feedlist;
+        this.context = cont;
+    }
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup viewGroup, int i){
+        View view = LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.main_cardview,null);
+        ViewHolder viewHolder = new ViewHolder(view);
+        return viewHolder;
+    }
+    @Override
+    public void onBindViewHolder (ViewHolder viewHolder, int i){
+        FeedItem feedItem = feedItemList.get(i);
+        viewHolder.artist.setText(feedItem.artist);
+        viewHolder.title.setText(feedItem.title);
+        viewHolder.date.setText(feedItem.date);
+        viewHolder.time.setText(feedItem.time);
+        viewHolder.location.setText(feedItem.location);
+        viewHolder.cost.setText(feedItem.cost);
+        viewHolder.desc.setText(feedItem.desc);
+    }
+    @Override
+    public int getItemCount(){
+        return (null != feedItemList ? feedItemList.size() : 0);
+    }
+    public static class ViewHolder extends RecyclerView.ViewHolder{
+        public TextView title;
+        public TextView artist;
+        public TextView date;
+        public TextView time;
+        public TextView location;
+        public TextView cost;
+        public TextView desc;
+        public ViewHolder (View view){
+            super(view);
+            title =  (TextView) view.findViewById(R.id.cardTitle);
+            artist =  (TextView) view.findViewById(R.id.cardBandName);
+            date =  (TextView) view.findViewById(R.id.cardDate);
+            time =  (TextView) view.findViewById(R.id.cardTimeText);
+            location =  (TextView) view.findViewById(R.id.cardVenueText);
+            cost =  (TextView) view.findViewById(R.id.cardCost);
+            desc  =  (TextView) view.findViewById(R.id.cardDesc);
+
+        }
+
+    }
+}
---
+++ LocalMusicScene/app/src/main/java/lemonapps/localmusicscene/FeedItem.java
@@ -0,0 +1,17 @@
+package lemonapps.localmusicscene;
+
+
+
+/**
+ * Created by Rob on 2017-03-14.
+ */
+
+public class FeedItem {
+    public String title;
+    public String artist;
+    public String date;
+    public String time;
+    public String location;
+    public String cost;
+    public String desc;
+}
