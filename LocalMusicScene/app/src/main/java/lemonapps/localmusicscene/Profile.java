ae271b8c23bc5dd871ee043dce497afa50eb3c4d
SteveArias <steveandrearias@gmail.com>
2017-03-14 14:10:36 -0400

    stuff

--- LocalMusicScene/app/src/main/java/lemonapps/localmusicscene/Profile.java
+++ LocalMusicScene/app/src/main/java/lemonapps/localmusicscene/Profile.java
@@ -1,8 +1,12 @@
 package lemonapps.localmusicscene;
 
-/**
- * Created by steve on 2017-03-14.
- */
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
-public class Profile {
+public class Profile extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.my_profile);
+    }
 }
