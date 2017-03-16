2d858532df482c1cf12fb3a7b69af167a57f7c87
SteveArias <steveandrearias@gmail.com>
2017-03-16 14:48:09 -0400

    Button Change
    When you press "Change Password" on the my_profile.xml it go to the
    change_pass.xml

--- LocalMusicScene/app/src/main/java/lemonapps/localmusicscene/Profile.java
+++ LocalMusicScene/app/src/main/java/lemonapps/localmusicscene/Profile.java
@@ -1,7 +1,10 @@
 package lemonapps.localmusicscene;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Profile extends AppCompatActivity {
 
@@ -9,5 +12,18 @@ public class Profile extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.my_profile);
+
+        Button profChgPassButton = (Button)findViewById(R.id.profchkpass);
+
+        profChgPassButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(Profile.this, ChangePasswords.class);
+                startActivity(i);
+
+            }
+        });
     }
+
+
 }
