39f84afd0cd0bdb5662b2e77bfbf6e3b294e7ca2
marshmelllo <robfarlow@gmail.com>
2017-03-16 22:52:09 -0400

    DATE for events

--- LocalMusicScene/app/src/main/java/lemonapps/localmusicscene/AddEvent.java
+++ LocalMusicScene/app/src/main/java/lemonapps/localmusicscene/AddEvent.java
@@ -4,11 +4,17 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.EditText;
+import android.widget.TimePicker;
 import android.widget.Toast;
 
+import java.util.Calendar;
+
 public class AddEvent extends AppCompatActivity {
-    EditText eTitle,eArtist,eTime,eDate,eLocation,eDesc;
+    EditText eTitle,eArtist,eLocation,eDesc,eCost;
+    TimePicker eTime;
+    DatePicker eDate;
     Button btnAddEvent;
     SQLConnection con;
     @Override
@@ -16,18 +22,22 @@ public class AddEvent extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.add_event);
         con = new SQLConnection();
+        eCost = (EditText)findViewById(R.id.priceField);
         eTitle = (EditText)findViewById(R.id.showTitleField);
         eArtist = (EditText)findViewById(R.id.bandNameField);
-        eTime = (EditText)findViewById(R.id.timeField);
-        eDate = (EditText)findViewById(R.id.dateField);
+        eTime = (TimePicker) findViewById(R.id.timeField);
+        eDate = (DatePicker) findViewById(R.id.dateField);
         eLocation = (EditText)findViewById(R.id.venueField);
         eDesc = (EditText) findViewById(R.id.showDescField);
         btnAddEvent = (Button)findViewById(R.id.addEventButton);
+        final Calendar calendar = Calendar.getInstance();
+        calendar.set(eDate.getDayOfMonth(),eDate.getMonth(),eDate.getYear());
+        final String time = ""+eTime.getCurrentHour() + ":"+ eTime.getCurrentMinute();
         btnAddEvent.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if(con.saveEvent(eTitle.getText().toString(),eArtist.getText().toString(),eDate.getText().toString(),eTime.getText().toString(),eDate.getText().toString(),eLocation.getText().toString(),eDesc.getText().toString())){
-                    Toast.makeText(getApplicationContext(),"Event Saved",Toast.LENGTH_SHORT);
+                if(con.saveEvent(eTitle.getText().toString(),eArtist.getText().toString(),calendar.getTime(),time,eLocation.getText().toString(),eCost.getText().toString(),eDesc.getText().toString())){
+                    Toast.makeText(getApplicationContext(),"Event Saved",Toast.LENGTH_SHORT).show();
                     finish();
                 }
             }
